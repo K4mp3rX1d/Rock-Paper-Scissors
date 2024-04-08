862f06ac439ba695ce12b7f108727b153cdfbd50
K4mp3rX1d <141189533+K4mp3rX1d@users.noreply.github.com>
2024-04-09 02:44:18 +0600

    Fix playRound() and playGame() functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Functions
+// Function declarations
 function random(n) {
     return Math.floor(Math.random() * n);
 }
@@ -11,75 +11,40 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'rock') {
-            console.log('Tie!');
-            return [0, 0];
-        } else if (computerSelection === 'paper') {
-            console.log("You Lose! Paper beats Rock");
-            return [1, 0];
-        } else if (computerSelection === 'scissors') {
-            console.log("You Win! Rock beats Scissors");
-            return [0, 1];
-        }
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            console.log("You Win! Paper beats Rock");
-            return [0, 1];
-        } else if (computerSelection === 'paper') {
-            console.log("Tie!");
-            return [0, 0];
-        } else if (computerSelection === 'scissors') {
-            console.log("You Lose! Scissors cut Paper");
-            return [1, 0];
-        }
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            console.log("You Lose! Rock beats Scissors");
-            return [1, 0];
-        } else if (computerSelection === 'paper') {
-            console.log("You Win! Scissors cut Paper");
-            return [0, 1];
-        } else if (computerSelection === 'scissors') {
-            console.log("Tie!");
-            return [0, 0];
-        }
+    if (playerSelection === computerSelection) {
+        return'Tie!'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return'You Lose!! Paper beats Rock!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return'You Win!! Rock beats Scissors!'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return'You Win!! Paper beats Rock!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return'You Lose!! Scissors cut paper!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return'You Lose!! Rock beats Scissors!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return'You Win!! Scissors cut Paper!'
     }
 }
 
 
-function playGame() {
-    let computerScore = 0;
-    let playerScore = 0;
+function playGame(event) {
+    const playerSelection = event.target.innerHTML.toLowerCase();
+    const computerSelection = getComputerChoice();
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Enter your choice: ').toLowerCase();
-        const computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result[0] > result[1]) {
-            computerScore++;
-        } else if (result[0] < result[1]) {
-            playerScore++;
-        }
-    }
-    
     console.log(`
-        Computer:   ${computerScore}
-        Player  :   ${playerScore}
-
-        ${computerScore === playerScore ? 'The Game ended with a Tie!' :
-            computerScore > playerScore ? 'Computer won the Game!!' :
-                'You won the Game!!'}
+        Player:     ${playerSelection.toUpperCase()}
+        Computer:   ${computerSelection.toUpperCase()}
+        ${playRound(playerSelection, computerSelection)};
     `);
 }
 
 
-// HTML Elements
+// HTML Elements & Event Handlers
 buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        let computerSelection = getComputerChoice();
-        let playerSelection = event.target.innerHTML.toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
+        playGame(event);
     });
 });
